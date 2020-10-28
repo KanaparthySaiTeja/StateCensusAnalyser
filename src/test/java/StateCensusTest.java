151e7f61404e2306c18e2b32b400d9785da4d254
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-28 20:45:25 +0530

    delimiter

--- src/test/java/StateCensusTest.java
+++ src/test/java/StateCensusTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import java.io.IOException;
 
 public class StateCensusTest {
+
     public static final String STATECENSUS_CSVFILE= "C:\\Users\\USER\\Desktop\\files\\StateCensusData.csv";
     public static final String STATECENSUS_CSVFILEWRONGDELIMITER= "C:\\Users\\USER\\Desktop\\files\\StateCensusDatadelimiter.csv";
     public static final String STATECENSUS_CSVFILEWRONGHEADER= "C:\\Users\\USER\\Desktop\\files\\StateCensusDataheader.csv";
@@ -17,6 +18,35 @@ public class StateCensusTest {
             Assert.assertEquals(29, count);
         } catch (CensusException e) {
             e.printStackTrace();
+        }
+    }
+    @Test
+    public void GivenStateCensusCsvFile_If_DoesntExist_ShouldThrowCensusAnalyserException() throws IOException {
+        try {
+            int count = StateCensusAnalyser.openCsvBuilder(STATECENSUS_CSVFILEWRONGPATH, StateCensus.class);
+        } catch (CensusException e) {
+            e.printStackTrace();
+            Assert.assertEquals(CensusException.CensusExceptionType.NO_SUCH_FILE, e.type);
+        }
+    }
+
+    @Test
+    public void GivenTheStateCensusCsvFile_FileExtIncorrect_ShouldThrow_CensusException() throws IOException {
+        try {
+            int count = StateCensusAnalyser.openCsvBuilder(STATECENSUS_CSVFILEWRONGTYPE, StateCensus.class);
+        } catch (CensusException e) {
+            e.printStackTrace();
+            Assert.assertEquals(CensusException.CensusExceptionType.INCORRECT_DATA_ISSUE, e.type);
+        }
+    }
+    @Test
+    public void GivenStateCensusCSVFile_DelimiterIncorrect_ReturnsCensusException() throws IOException {
+        try {
+            int count = StateCensusAnalyser.openCsvBuilder(STATECENSUS_CSVFILEWRONGDELIMITER, StateCensus.class);
+        } catch (CensusException e) {
+            e.printStackTrace();
+            Assert.assertEquals(CensusException.CensusExceptionType.DELIMITER_ISSUE, e.type);
+
         }
     }
 }
