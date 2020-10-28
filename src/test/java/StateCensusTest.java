01718d80fd76cc2946981c8577ee545509ac77fc
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-28 20:29:50 +0530

    Welcome to State Census Analyser

--- src/test/java/StateCensusTest.java
+++ src/test/java/StateCensusTest.java
@@ -4,19 +4,5 @@ import org.junit.Test;
 import java.io.IOException;
 
 public class StateCensusTest {
-    public static final String STATECENSUS_CSVFILE= "C:\\Users\\USER\\Desktop\\files\\StateCensusData.csv";
-    public static final String STATECENSUS_CSVFILEWRONGDELIMITER= "C:\\Users\\USER\\Desktop\\files\\StateCensusDatadelimiter.csv";
-    public static final String STATECENSUS_CSVFILEWRONGHEADER= "C:\\Users\\USER\\Desktop\\files\\StateCensusDataheader.csv";
-    public static final String STATECENSUS_CSVFILEWRONGTYPE= "C:\\Users\\USER\\Desktop\\files\\statecensuspdf.pdf";
-    public static final String STATECENSUS_CSVFILEWRONGPATH= "C:\\Users\\USER\\StateCensusData.csv";
-    @Test
-    public void GivenTheStateCodesCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() throws IOException {
-        try {
-            int count = StateCensusAnalyser.openCsvBuilder(STATECENSUS_CSVFILE, StateCensus.class);
-            System.out.println(count);
-            Assert.assertEquals(29, count);
-        } catch (CensusException e) {
-            e.printStackTrace();
-        }
-    }
+
 }
